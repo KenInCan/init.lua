f5739b35781745cfbcd9320e35b7f0839179b82f
KenInCan <83908038+KenInCan@users.noreply.github.com>
2023-09-08 17:12:09 +0900

    Change: Changed and added few settings to my liking

--- lua/theprimeagen/init.lua
+++ lua/theprimeagen/init.lua
@@ -35,3 +35,7 @@ autocmd({"BufWritePre"}, {
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+
+-- run feline
+require('feline').setup()
+require('feline').winbar.setup()
